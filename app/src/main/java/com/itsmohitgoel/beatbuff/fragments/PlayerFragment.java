04c6e7b144387d39ac8e7f8a262745382a3090ca
Mohit Goel <itsmohitgoel@gmail.com>
2018-09-12 16:02:05 +0530

    feat: Show the NowPlaying screen as per the finalised UI.

--- app/src/main/java/com/itsmohitgoel/beatbuff/fragments/PlayerFragment.java
+++ app/src/main/java/com/itsmohitgoel/beatbuff/fragments/PlayerFragment.java
@@ -1,29 +1,46 @@
 package com.itsmohitgoel.beatbuff.fragments;
 
-import android.content.ContentResolver;
 import android.content.ContentUris;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.itsmohitgoel.beatbuff.R;
+import com.itsmohitgoel.beatbuff.utils.Blur;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PlayerFragment extends Fragment {
+
+    @BindView(R.id.google_cast_button)
+    ImageView googleCastButton;
+    @BindView(R.id.share_button)
+    ImageView shareButton;
+    @BindView(R.id.equalizer_button)
+    ImageView equalizerButton;
+    @BindView(R.id.repeat_button)
+    ImageView repeatButton;
+    @BindView(R.id.shuffle_button)
+    ImageView shuffleButton;
+    @BindView(R.id.playlist_button)
+    ImageView playlistButton;
+
+
     public static PlayerFragment newInstance() {
         return new PlayerFragment();
     }
@@ -35,13 +52,32 @@ public class PlayerFragment extends Fragment {
         View view = inflater
                 .inflate(R.layout.fragment_player, container, false);
 
+        initializeUI(view);
+
         CircleImageView profileImageView = (CircleImageView) view.findViewById(R.id
                 .circle_image_view);
 
-        profileImageView.setImageBitmap(getAlbumArtImage(R.raw.faded_by_alan_walker));
+        Bitmap albumArtImage = getAlbumArtImage(R.raw.cheap_thrills_by_sia);
+        profileImageView.setImageBitmap(albumArtImage);
+
+        Bitmap albumArtBlurImage = Blur.fastblur(getActivity(), albumArtImage, 12);
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(albumArtBlurImage);
+        view.setBackgroundDrawable(bitmapDrawable);
+        view.getBackground().setColorFilter(Blur.getGrayScaleFilter());
+
         return view;
     }
 
+    private void initializeUI(View view) {
+        ButterKnife.bind(this, view);
+
+        googleCastButton.setOnClickListener(controlButtonsListener);
+        shareButton.setOnClickListener(controlButtonsListener);
+        equalizerButton.setOnClickListener(controlButtonsListener);
+        repeatButton.setOnClickListener(controlButtonsListener);
+        shuffleButton.setOnClickListener(controlButtonsListener);
+        playlistButton.setOnClickListener(controlButtonsListener);
+    }
 
     /**
      * Extract the Album cover from the audio resource file itself
@@ -58,4 +94,44 @@ public class PlayerFragment extends Fragment {
 
         return image;
     }
+
+    private View.OnClickListener controlButtonsListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.google_cast_button:
+                    setBackgroundTint(googleCastButton.getDrawable());
+                    break;
+
+                case R.id.share_button:
+                    setBackgroundTint(shareButton.getDrawable());
+                    break;
+
+                case R.id.equalizer_button:
+                    setBackgroundTint(equalizerButton.getDrawable());
+                    break;
+
+                case R.id.repeat_button:
+                    setBackgroundTint(repeatButton.getDrawable());
+                    break;
+
+                case R.id.shuffle_button:
+                    setBackgroundTint(shuffleButton.getDrawable());
+                    break;
+
+                case R.id.playlist_button:
+                    setBackgroundTint(playlistButton
+                            .getDrawable());
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void setBackgroundTint(Drawable drawable) {
+            DrawableCompat.setTint(drawable,
+                    ContextCompat.getColor(getActivity(), R.color.cyan_light));
+        }
+    };
 }
