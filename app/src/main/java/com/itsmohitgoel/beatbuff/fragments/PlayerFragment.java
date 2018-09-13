2f8ba78045b2275599f880bf984276e7cacf0a21
Mohit Goel <itsmohitgoel@gmail.com>
2018-09-13 11:32:56 +0530

    feat: Will play a specifiic song on clicking play button.

--- app/src/main/java/com/itsmohitgoel/beatbuff/fragments/PlayerFragment.java
+++ app/src/main/java/com/itsmohitgoel/beatbuff/fragments/PlayerFragment.java
@@ -13,19 +13,23 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.itsmohitgoel.beatbuff.R;
+import com.itsmohitgoel.beatbuff.listeners.PlayerAdapter;
 import com.itsmohitgoel.beatbuff.utils.Blur;
+import com.itsmohitgoel.beatbuff.utils.MediaPlayerHolder;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PlayerFragment extends Fragment {
+    private static final String TAG = PlayerFragment.class.getSimpleName();
 
     @BindView(R.id.google_cast_button)
     ImageView googleCastButton;
@@ -39,6 +43,14 @@ public class PlayerFragment extends Fragment {
     ImageView shuffleButton;
     @BindView(R.id.playlist_button)
     ImageView playlistButton;
+    @BindView(R.id.background_image_view)
+    ImageView backgroundImageView;
+    @BindView(R.id.circle_image_view)
+    CircleImageView profileImageView;
+    @BindView(R.id.play_button)
+    ImageView playButton;
+
+    private PlayerAdapter mPlayerAdapter;
 
 
     public static PlayerFragment newInstance() {
@@ -53,46 +65,15 @@ public class PlayerFragment extends Fragment {
                 .inflate(R.layout.fragment_player, container, false);
 
         initializeUI(view);
-
-        CircleImageView profileImageView = (CircleImageView) view.findViewById(R.id
-                .circle_image_view);
-
-        Bitmap albumArtImage = getAlbumArtImage(R.raw.cheap_thrills_by_sia);
-        profileImageView.setImageBitmap(albumArtImage);
-
-        Bitmap albumArtBlurImage = Blur.fastblur(getActivity(), albumArtImage, 12);
-        BitmapDrawable bitmapDrawable = new BitmapDrawable(albumArtBlurImage);
-        view.setBackgroundDrawable(bitmapDrawable);
-        view.getBackground().setColorFilter(Blur.getGrayScaleFilter());
+        initializePlaybackController();
 
         return view;
     }
 
-    private void initializeUI(View view) {
-        ButterKnife.bind(this, view);
-
-        googleCastButton.setOnClickListener(controlButtonsListener);
-        shareButton.setOnClickListener(controlButtonsListener);
-        equalizerButton.setOnClickListener(controlButtonsListener);
-        repeatButton.setOnClickListener(controlButtonsListener);
-        shuffleButton.setOnClickListener(controlButtonsListener);
-        playlistButton.setOnClickListener(controlButtonsListener);
-    }
-
-    /**
-     * Extract the Album cover from the audio resource file itself
-     */
-    private Bitmap getAlbumArtImage(long resId) {
-        Uri baseUriForAudioFiles = Uri.parse("android.resource://" + getActivity().getPackageName() + "/raw");
-        Uri uri = ContentUris.withAppendedId(baseUriForAudioFiles, resId);
-        Bitmap image;
-
-        MediaMetadataRetriever mData = new MediaMetadataRetriever();
-        mData.setDataSource(getActivity(), uri);
-        byte art[] = mData.getEmbeddedPicture();
-        image = BitmapFactory.decodeByteArray(art, 0, art.length);
-
-        return image;
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPlayerAdapter.loadMedia(R.raw.cheap_thrills_by_sia);
     }
 
     private View.OnClickListener controlButtonsListener = new View.OnClickListener() {
@@ -124,6 +105,8 @@ public class PlayerFragment extends Fragment {
                             .getDrawable());
                     break;
 
+                case R.id.play_button:
+                    mPlayerAdapter.play();
                 default:
                     break;
             }
@@ -134,4 +117,46 @@ public class PlayerFragment extends Fragment {
                     ContextCompat.getColor(getActivity(), R.color.cyan_light));
         }
     };
+
+    private void initializeUI(View view) {
+        ButterKnife.bind(this, view);
+
+        Bitmap albumArtImage = getAlbumArtImage(R.raw.cheap_thrills_by_sia);
+        profileImageView.setImageBitmap(albumArtImage);
+
+        Bitmap albumArtBlurImage = Blur.fastblur(getActivity(), albumArtImage, 12);
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(albumArtBlurImage);
+        backgroundImageView.setImageBitmap(albumArtBlurImage);
+        backgroundImageView.setColorFilter(Blur.getGrayScaleFilter());
+
+        googleCastButton.setOnClickListener(controlButtonsListener);
+        shareButton.setOnClickListener(controlButtonsListener);
+        equalizerButton.setOnClickListener(controlButtonsListener);
+        repeatButton.setOnClickListener(controlButtonsListener);
+        shuffleButton.setOnClickListener(controlButtonsListener);
+        playlistButton.setOnClickListener(controlButtonsListener);
+        playButton.setOnClickListener(controlButtonsListener);
+    }
+
+    /**
+     * Extract the Album cover from the audio resource file itself
+     */
+    private Bitmap getAlbumArtImage(long resId) {
+        Uri baseUriForAudioFiles = Uri.parse("android.resource://" + getActivity().getPackageName() + "/raw");
+        Uri uri = ContentUris.withAppendedId(baseUriForAudioFiles, resId);
+        Bitmap image;
+
+        MediaMetadataRetriever mData = new MediaMetadataRetriever();
+        mData.setDataSource(getActivity(), uri);
+        byte art[] = mData.getEmbeddedPicture();
+        image = BitmapFactory.decodeByteArray(art, 0, art.length);
+
+        return image;
+    }
+
+    private void initializePlaybackController() {
+        MediaPlayerHolder mediaPlayerHolder = new MediaPlayerHolder(getActivity());
+        Log.d(TAG, "initializePlaybackController: created MediaPlayerHolder");
+        mPlayerAdapter = mediaPlayerHolder;
+    }
 }
--- app/src/main/java/com/itsmohitgoel/beatbuff/utils/MediaPlayerHolder.java
+++ app/src/main/java/com/itsmohitgoel/beatbuff/utils/MediaPlayerHolder.java
@@ -1,5 +1,91 @@
 package com.itsmohitgoel.beatbuff.utils;
 
-public class MediaPlayerHolder {
-    
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.Log;
+
+import com.itsmohitgoel.beatbuff.R;
+import com.itsmohitgoel.beatbuff.listeners.PlayerAdapter;
+
+import java.io.IOException;
+
+public class MediaPlayerHolder implements PlayerAdapter{
+    private static final String TAG = MediaPlayerHolder.class.getSimpleName();
+
+    private final Context mContext;
+    private MediaPlayer mMediaPlayer;
+    private int mResourceId;
+
+    public MediaPlayerHolder(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    @Override
+    public void loadMedia(int resourceId) {
+        mResourceId = resourceId;
+
+        initializeMediaPlayer();
+
+        Uri baseUriForAudioFiles = Uri.parse("android.resource://" + mContext.getPackageName() + "/raw");
+        Uri uri = ContentUris.withAppendedId(baseUriForAudioFiles, mResourceId);
+        try {
+            Log.i(TAG, "load() {1. setDataSource}");
+            mMediaPlayer.setDataSource(mContext, uri);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot load data source in player: setDataSource() failed", ioe);
+        }
+
+        try {
+            Log.i(TAG, "load() {2. prepare}");
+            mMediaPlayer.prepare();
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot prepare mMediaPlayer: prepare() failed", ioe);
+        }
+
+    }
+
+    @Override
+    public void release() {
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public void play() {
+        mMediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void initializeProgressCallback() {
+
+    }
+
+    @Override
+    public void seekTo(int position) {
+
+    }
+
+    private void initializeMediaPlayer() {
+        if (mMediaPlayer == null) {
+            mMediaPlayer = new MediaPlayer();
+
+            Log.i(TAG, "mMediaPlayer = new MediaPlayer()");
+        }
+    }
 }
